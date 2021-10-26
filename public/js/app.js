7831ae14dcd915d1d47e4f3826f1807a4d9de7e9
irwndandka <irwndandka@gmail.com>
2021-10-26 11:04:26 +0700

    update style and script home menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,14 @@
 // var Turbolinks = require("turbolinks")
+
+const { data } = require("jquery");
+
 // Turbolinks.start()
+function copyNumber(){
+    let dataNumber = document.querySelector(".dataNumber");
+    dataNumber.select();
+    dataNumber.setSelectionRange(0, 99999); /* For mobile devices */
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(dataNumber.value);
+    /* Alert the copied text */
+    alert("Tersalin");
+}
